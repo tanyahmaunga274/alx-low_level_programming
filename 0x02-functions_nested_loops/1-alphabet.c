a4238c9c5b681ee3b6c5d2afc84db6719daf2234
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2022-07-05 17:13:00 -0700

    Amend the comment in file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- *   */
+ * print_alphabet - prints the alphabet, in lowercase,
+ *  followed by a new line
+ */
 void print_alphabet(void)
 {
 	char ch;
