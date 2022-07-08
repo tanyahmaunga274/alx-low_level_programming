74b3b43b8fc4b0a6de7b6115a17a343f7df4490d
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2022-07-07 20:00:05 -0700

    fix typo

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line
+ * print_triangle - prints a triangle, 
+ * followed by a new line
  * @size: size of the triangle
  */
 void print_triangle(int size)
