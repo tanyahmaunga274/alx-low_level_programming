797dc5000ae2f156097ad8ed1d917e044b0fdc8a
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2022-07-15 03:05:41 -0700

    Fix typo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,8 @@
  * @s1: first string to compare
  * @s2: second string to compare
  *
- * Return: less than 0 if s1 is less than
- * s2, 0 if they are equal,
- * more than 0 s1 is greater than s2
+ * Return: less than 0 if s1 is less than s2, if they are equal
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
