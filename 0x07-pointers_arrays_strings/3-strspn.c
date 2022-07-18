ad3ea1f28ae32a3c555b3a16d4454dead33a0d93
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2022-07-18 08:01:38 -0700

    Fix typo in main.h

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
-unsigned int *_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
